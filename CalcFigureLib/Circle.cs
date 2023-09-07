b8c9fb819077f9889959cf5f3fb6496a431152e3
SanchezM <eligar_redeson@mail.ru>
2023-09-07 16:06:27 +0300

    add calc square of circle

---
+++ CalcFigureLib/Circle.cs
@@ -0,0 +1,20 @@
+﻿namespace CalcFigureLib
+{
+    public class Circle : IFigure
+    {
+
+        public double _Radius { get;}
+
+        public Circle(double Radius) 
+        {
+            if (Radius <= 0)
+                throw new ArgumentException($"{nameof(Radius)} не может быть равен или отрицателен нулю");
+            _Radius = Radius;
+        }
+
+        public double GetSqure()
+        {
+            return Math.PI * Math.Pow(_Radius, 2);
+        }
+    }
+}
